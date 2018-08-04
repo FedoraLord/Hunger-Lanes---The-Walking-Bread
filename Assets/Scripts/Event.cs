869fbda89683811401d154b2d0d8ecfcd6501085
FedoraLord <jerkrouse@gmail.com>
2018-08-04 00:16:01 -0500

    Got the scene set up and the data interface for the dialogue system

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+public class Event : System.Object
+{
+    public string eventName;
+    public Sprite background;
+    public Dialogue[] dialogues;
+    public Decision[] decisions;
+}
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public GameObject titleScreen;
+    public GameObject gameScreen;
+
+    public Event[] events;
+
+    public void PlayGame()
+    {
+        titleScreen.SetActive(false);
+        gameScreen.SetActive(true);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
